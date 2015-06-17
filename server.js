f87b7b1778d122dc744c824cd7b608ef3336f67f
Jason DeTiberus <jdetiber@redhat.com>
2015-06-17 18:23:17 -0400

    that didn't work

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ var LRU = require("lru-cache")
               , length: function (n) { return n.length }
               , maxAge: 1000 }
   , podCache = LRU(options)
-  , serviceCache = LRU(1)
   ;
 
 http.globalAgent.maxSockets = Infinity;
@@ -34,8 +33,6 @@ var config = {
   openshiftServer: 'https://' + (process.env.OPENSHIFT_SERVER || 'openshift-master.summit.paas.ninja:8443')
 , port: process.env.OPENSHIFT_NODEJS_PORT || 8080
 , hostname: process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
-, appNamespace: process.env.APP_NAMESPACE || 'demo'
-, appService: process.env.APP_SERVICE || 'sketchpod'
 };
 
 var re = /^\/([a-z0-9\-]*)\/([a-z0-9\-]*)/;
@@ -47,43 +44,6 @@ function proxy_request(proxy, req, res, options){
 var server = http.createServer(function(req, res) {
   if (req.url.indexOf('/api/v1beta3/namespaces/') == 0) {
     proxy_request(proxy, req, res, { target: config.openshiftServer });
-  } else if (req.url.indexOf('/' + config.appNamespace) !== 0) {
-    proxy_request(proxy, req, res, { target: config.openshiftServer });
-    var client = restify.createJsonClient({
-      url: config.openshiftServer,
-      rejectUnauthorized: false,
-      headers: {
-        authorization: "Bearer " + token
-      }
-    });
-    var serviceUrl = podCache.get(config.appService);
-    if (!serviceUrl) {
-      var servicesPath = "/api/v1beta3/namespaces/" + config.appNamespace + "/services/" + config.appService;
-      var servicesUrl = config.openshiftServer + servicesPath
-      client.get(servicesUrl, function(err, c_req, c_res, obj) {
-        if (err instanceof Error) {
-          console.log("Error querying api: ", err)
-          if (!res.headersSent) {
-            res.writeHead(500, { 'content-type': 'application/json' });
-          }
-          var json = { error: 'proxy_error', reason: error.message };
-          res.end(JSON.stringify(json));
-        } else {
-          var serviceIp = obj.spec.portalIP;
-	  var servicePort = obj.spec.ports[0].port
-          var serviceUrl = "http://" + serviceIp + ":" + servicePort;
-          console.log("Caching value: " + serviceUrl + " for: " + config.appService);
-          podCache.set(config.appService, serviceUrl);
-          console.log(req.url);
-	  proxy_request(proxy, req, res, { target: serviceUrl });
-        }
-      });
-    } else {
-      console.log("Using cached value: " + serviceUrl + " for: " + config.appService);
-      req.url = newPath;
-      console.log(req.url);
-      proxy_request(proxy, req, res, { target: containerUrl });
-    }
   } else {
     var parsed = url.parse(req.url);
     console.log("parsed: ", parsed)
