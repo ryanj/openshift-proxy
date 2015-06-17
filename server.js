ac23b2d42a6a0fb56ba3d98d0b0471edb464ac8e
Jason DeTiberus <jdetiber@redhat.com>
2015-06-17 17:31:05 -0400

    send assets to service

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ var LRU = require("lru-cache")
   , options = { max: 1026
               , length: function (n) { return n.length }
               , maxAge: 1000 }
-  , cache = LRU(options)
+  , podCache = LRU(options)
+  , serviceCache = LRU(1)
   ;
 
 http.globalAgent.maxSockets = Infinity;
@@ -33,6 +34,8 @@ var config = {
   openshiftServer: 'https://' + (process.env.OPENSHIFT_SERVER || 'openshift-master.summit.paas.ninja:8443')
 , port: process.env.OPENSHIFT_NODEJS_PORT || 8080
 , hostname: process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+, appNamespace: process.env.APP_NAMESPACE || 'demo'
+, appService: process.env.APP_SERVICE || 'sketchpod'
 };
 
 var re = /^\/([a-z0-9\-]*)\/([a-z0-9\-]*)/;
@@ -42,7 +45,46 @@ function proxy_request(proxy, req, res, options){
 }
 
 var server = http.createServer(function(req, res) {
-  if (req.url.indexOf('/api/v1beta3/namespaces/') !== 0) {
+  if (req.url.indexOf('/api/v1beta3/namespaces/') == 0) {
+    proxy_request(proxy, req, res, { target: config.openshiftServer });
+  } else if (req.url.indexOf('/' + config.appNamespace) !== 0) {
+    proxy_request(proxy, req, res, { target: config.openshiftServer });
+    var client = restify.createJsonClient({
+      url: config.openshiftServer,
+      rejectUnauthorized: false,
+      headers: {
+        authorization: "Bearer " + token
+      }
+    });
+    var serviceUrl = podCache.get(config.appService);
+    if (!serviceUrl) {
+      var servicesPath = "/api/v1beta3/namespaces/" + config.appNamespace + "/services/" + config.appService;
+      var servicesUrl = config.openshiftServer + servicesPath
+      client.get(, function(err, c_req, c_res, obj) {
+        if (err instanceof Error) {
+          console.log("Error querying api: ", err)
+          if (!res.headersSent) {
+            res.writeHead(500, { 'content-type': 'application/json' });
+          }
+          var json = { error: 'proxy_error', reason: error.message };
+          res.end(JSON.stringify(json));
+        } else {
+          var serviceIp = obj.spec.portalIP;
+	  var servicePort = obj.spec.ports[0].port
+          var serviceUrl = "http://" + serviceIp + ":" + servicePort;
+          console.log("Caching value: " + serviceUrl + " for: " + config.appService);
+          podCache.set(config.appService, serviceUrl);
+          console.log(req.url);
+	  proxy_request(proxy, req, res, { target: serviceUrl });
+        }
+      }
+    } else {
+      console.log("Using cached value: " + serviceUrl + " for: " + config.appService);
+      req.url = newPath;
+      console.log(req.url);
+      proxy_request(proxy, req, res, { target: containerUrl });
+    }
+  } else {
     var parsed = url.parse(req.url);
     console.log("parsed: ", parsed)
     var results = parsed.pathname.match(re);
@@ -53,14 +95,10 @@ var server = http.createServer(function(req, res) {
     var pod = results[2];
     console.log("pod: ", pod)
     var newPath = results.slice(3).join('/')
-    if (newPath == ''){
-      newPath = "/";
-    }
     console.log("newPath: ", newPath)
     if (results) {
-
       var cacheKey = namespace + "/" + pod;
-      var containerUrl = cache.get(cacheKey);
+      var containerUrl = podCache.get(cacheKey);
       if (!containerUrl) {
         var client = restify.createJsonClient({
           url: config.openshiftServer,
@@ -73,7 +111,6 @@ var server = http.createServer(function(req, res) {
 	var podUrl = config.openshiftServer + podPath
         console.log("podUrl: ", podUrl)
         client.get(podUrl, function(err, c_req, c_res, obj) {
-	  console.log
           if (err instanceof Error) {
             console.log("Error querying api: ", err)
             console.log("Failing back to kube proxy");
@@ -87,7 +124,7 @@ var server = http.createServer(function(req, res) {
             var containerPort = obj.spec.containers[0].ports[0].containerPort;
             var containerUrl = "http://" + podIp + ":" + containerPort;
             console.log("Caching value: " + containerUrl + " for: " + cacheKey);
-            cache.set(cacheKey, containerUrl);
+            podCache.set(cacheKey, containerUrl);
 	    req.url = newPath;
             console.log(req.url);
 	    proxy_request(proxy, req, res, { target: containerUrl });
