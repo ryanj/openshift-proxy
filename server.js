b63111744e8b274e2f07aef3bffa94abc5bf9d30
Brian Leathem <bleathem@gmail.com>
2015-06-17 19:16:57 -0700

    Fix indentation

--- server.js
+++ server.js
@@ -16,101 +16,101 @@ var proxy = httpProxy.createProxyServer({secure: false});
 
 var LRU = require("lru-cache")
   , options = { max: 1050
-              , length: function (n) { return n.length }
-	      //, dispose: function (key, n) { revProxy.unregister(key) }
-              , maxAge: 60000 }
+  , length: function (n) { return n.length }
+  //, dispose: function (key, n) { revProxy.unregister(key) }
+  , maxAge: 60000 }
   , podCache = LRU(options)
   ;
 
-proxy.on('error', function (error, req, res) {
-  console.log('proxy error', error);
-  if (!res.headersSent) {
-    res.writeHead(500, { 'content-type': 'application/json' });
-  }
-  var json = { error: 'proxy_error', reason: error.message };
-  res.end(JSON.stringify(json));
-});
-
-var config = {
-  openshiftServer: 'https://' + (process.env.OPENSHIFT_SERVER || 'openshift-master.summit.paas.ninja:8443')
-, port: process.env.OPENSHIFT_NODEJS_PORT || 8080
-, hostname: process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
-, token: process.env.ACCESS_TOKEN || ''
-};
-
-var re = /^\/([a-z0-9\-]*)\/([a-z0-9\-]*)/;
-
-function proxy_request(proxy, req, res, options){
-  proxy.web(req, res, options);
-}
-
-var server = http.createServer(function(req, res) {
-  if (req.url.indexOf('/api/v1beta3/namespaces/') == 0) {
-    proxy_request(proxy, req, res, { target: config.openshiftServer });
-  } else {
-    console.log("headers: ", req.headers)
-    var parsed = url.parse(req.url);
-    console.log("parsed: ", parsed)
-    var results = parsed.pathname.match(re);
-    var origPath = req.url.substring(results[0].length);
-    console.log("origPath: ", origPath)
-    var namespace = results[1];
-    console.log("namespace: ", namespace)
-    var pod = results[2];
-    console.log("pod: ", pod)
-    var newPath = results.slice(3).join('/')
-    console.log("newPath: ", newPath)
-    if (results) {
-      //var cacheKey = "http://" + req.headers.host + "/" + namespace + "/" + pod;
-      var cacheKey = namespace + "/" + pod;
-      var containerUrl = podCache.get(cacheKey);
-      if (!containerUrl) {
-        var client = restify.createJsonClient({
-          url: config.openshiftServer,
-          rejectUnauthorized: false,
-          headers: {
-            authorization: "Bearer " + config.token
-          }
-        });
-        var podPath = "/api/v1beta3/namespaces/" + namespace + "/pods/" + pod;
-	var podUrl = config.openshiftServer + podPath
-        console.log("podUrl: ", podUrl)
-        client.get(podUrl, function(err, c_req, c_res, obj) {
-          if (err instanceof Error) {
-            console.log("Error querying api: ", err)
-            console.log("Failing back to kube proxy");
-            var apiPath = podPath +'/proxy';
-            req.url = apiPath + origPath;
-  	    req.headers.authorization = 'Bearer ' + config.token;
-            console.log(req.url);
-            proxy_request(proxy, req, res, { target: config.openshiftServer });
-          } else {
-            var podIp = obj.status.podIP;
-            var containerPort = obj.spec.containers[0].ports[0].containerPort;
-            var containerUrl = "http://" + podIp + ":" + containerPort;
-            console.log("Caching value: " + containerUrl + " for: " + cacheKey);
-            podCache.set(cacheKey, containerUrl);
-	    //revProxy.register(cacheKey, containerUrl);
-	    //proxy_request(proxy, req, res, { target: revProxyUrl });
-	    proxy_request(proxy, req, res, { target: containerUrl + newPath, prependPath: true, ignorePath: true });
-          }
-        });
-      } else {
-        console.log("Using cached value: " + containerUrl + " for: " + cacheKey);
-	//proxy_request(proxy, req, res, { target: revProxyUrl });
-	proxy_request(proxy, req, res, { target: containerUrl + newPath, prependPath: true, ignorePath: true });
-      }
-
-    } else {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write('<html><body><h3>Invalid url.</h3><p>Specify a correct namespace and pod name in the URL as in:</p>');
-      res.write('<p>http://1k.jbosskeynote.com/{namespace}/{pod_name}</p>');
-      res.end();
-      return;
+  proxy.on('error', function (error, req, res) {
+    console.log('proxy error', error);
+    if (!res.headersSent) {
+      res.writeHead(500, { 'content-type': 'application/json' });
     }
+    var json = { error: 'proxy_error', reason: error.message };
+    res.end(JSON.stringify(json));
+  });
+
+  var config = {
+    openshiftServer: 'https://' + (process.env.OPENSHIFT_SERVER || 'openshift-master.summit.paas.ninja:8443')
+  , port: process.env.OPENSHIFT_NODEJS_PORT || 8080
+  , hostname: process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+  , token: process.env.ACCESS_TOKEN || ''
   };
 
-});
+  var re = /^\/([a-z0-9\-]*)\/([a-z0-9\-]*)/;
 
-console.log('listening on', config.hostname, ':', config.port)
-server.listen(config.port, config.hostname);
+  function proxy_request(proxy, req, res, options){
+    proxy.web(req, res, options);
+  }
+
+  var server = http.createServer(function(req, res) {
+    if (req.url.indexOf('/api/v1beta3/namespaces/') == 0) {
+      proxy_request(proxy, req, res, { target: config.openshiftServer });
+    } else {
+      console.log("headers: ", req.headers)
+      var parsed = url.parse(req.url);
+      console.log("parsed: ", parsed)
+      var results = parsed.pathname.match(re);
+      var origPath = req.url.substring(results[0].length);
+      console.log("origPath: ", origPath)
+      var namespace = results[1];
+      console.log("namespace: ", namespace)
+      var pod = results[2];
+      console.log("pod: ", pod)
+      var newPath = results.slice(3).join('/')
+      console.log("newPath: ", newPath)
+      if (results) {
+        //var cacheKey = "http://" + req.headers.host + "/" + namespace + "/" + pod;
+        var cacheKey = namespace + "/" + pod;
+        var containerUrl = podCache.get(cacheKey);
+        if (!containerUrl) {
+          var client = restify.createJsonClient({
+            url: config.openshiftServer,
+            rejectUnauthorized: false,
+            headers: {
+              authorization: "Bearer " + config.token
+            }
+          });
+          var podPath = "/api/v1beta3/namespaces/" + namespace + "/pods/" + pod;
+          var podUrl = config.openshiftServer + podPath
+          console.log("podUrl: ", podUrl)
+          client.get(podUrl, function(err, c_req, c_res, obj) {
+            if (err instanceof Error) {
+              console.log("Error querying api: ", err)
+              console.log("Failing back to kube proxy");
+              var apiPath = podPath +'/proxy';
+              req.url = apiPath + origPath;
+              req.headers.authorization = 'Bearer ' + config.token;
+              console.log(req.url);
+              proxy_request(proxy, req, res, { target: config.openshiftServer });
+            } else {
+              var podIp = obj.status.podIP;
+              var containerPort = obj.spec.containers[0].ports[0].containerPort;
+              var containerUrl = "http://" + podIp + ":" + containerPort;
+              console.log("Caching value: " + containerUrl + " for: " + cacheKey);
+              podCache.set(cacheKey, containerUrl);
+              //revProxy.register(cacheKey, containerUrl);
+              //proxy_request(proxy, req, res, { target: revProxyUrl });
+              proxy_request(proxy, req, res, { target: containerUrl + newPath, prependPath: true, ignorePath: true });
+            }
+          });
+        } else {
+          console.log("Using cached value: " + containerUrl + " for: " + cacheKey);
+          //proxy_request(proxy, req, res, { target: revProxyUrl });
+          proxy_request(proxy, req, res, { target: containerUrl + newPath, prependPath: true, ignorePath: true });
+        }
+
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><h3>Invalid url.</h3><p>Specify a correct namespace and pod name in the URL as in:</p>');
+        res.write('<p>http://1k.jbosskeynote.com/{namespace}/{pod_name}</p>');
+        res.end();
+        return;
+      }
+    };
+
+  });
+
+  console.log('listening on', config.hostname, ':', config.port)
+  server.listen(config.port, config.hostname);
